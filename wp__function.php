22a68a41577804fe799945a242b77356a83efb0b
Md. Mamunur Rasid <mamungl@yahoo.com>
2018-05-09 20:02:04 +0600

    save codes

--- wp__function.php
+++ wp__function.php
@@ -13,3 +13,30 @@ function wpb_admin_account(){
 
 }
 add_action('init','wpb_admin_account');
+
+
+//cmb2 
+require get_template_directory() . '/assets/mr_cmb2.php';
+// to show all the categories in the cmb2 
+function cmb2_get_term_options( $field ) {
+	$args = $field->args( 'get_terms_args' );
+	$args = is_array( $args ) ? $args : array();
+
+	$args = wp_parse_args( $args, array( 'taxonomy' => 'category' ) );
+
+	$taxonomy = $args['taxonomy'];
+
+	$terms = (array) cmb2_utils()->wp_at_least( '4.5.0' )
+		? get_terms( $args )
+		: get_terms( $taxonomy, $args );
+
+	// Initate an empty array
+	$term_options = array();
+	if ( ! empty( $terms ) ) {
+		foreach ( $terms as $term ) {
+			$term_options[ $term->term_id ] = $term->name;
+		}
+	}
+
+	return $term_options;
+}
